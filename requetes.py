cbcfa1e050a1d8e2b4ca531c1c189195aa10ef85
alexy17 <alexy.wiciak@etu.univ-orleans.fr>
2024-04-12 13:41:41 +0200

    t

--- requetes.py
+++ requetes.py
@@ -23,19 +23,4 @@ def txt_to_json(fichier_txt, nouveau_fichier):
     except:
         print("Désolé nous rencontrons une erreur, le fichier n'existe pas ou est ilisible")
 
-# txt_to_json("data.txt", "data.json")
-
-#6.2 
-
-def collaborateurs_communs(G, u, v):
-    collaborateurs = set()
-    json_file = open(G, "r")
-    for film in json_file:
-        cast = film['cast']
-        if u in cast and v in cast:
-            for actor in cast:
-                if actor != u and actor != v:
-                    collaborateurs.add(actor)
-    return list(collaborateurs)
-
-print(collaborateurs_communs("data.json", "Bruce Campbell", "Ian Abercrombie"))
+txt_to_json("data.txt", "data.json")
